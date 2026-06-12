af3fae049461e9cba4698a5014986519b1e1e9df
kattyph <katty.philco17@gmail.com>
2026-06-12 15:42:45 -0500

    feat(shared): add appdbcontext skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/Shared/Infrastructure/Persistence/EntityFrameworkCore/Configuration/AppDbContext.cs
@@ -0,0 +1,21 @@
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddInterceptors(new AuditableEntityInterceptor());
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
