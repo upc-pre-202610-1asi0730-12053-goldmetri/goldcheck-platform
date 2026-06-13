90449f9924d62080348a710df90576b9488bf88c
kattyph <katty.philco17@gmail.com>
2026-06-12 19:06:18 -0500

    feat(iam): add iuserrepository interface.

---
+++ GoldMetrics.GoldCheck.Platform/Iam/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Aggregates;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> FindByIdAsync(int id);
+    Task<User?> FindByUsernameAsync(string username);
+    Task<User?> FindByEmailAsync(string email);
+    Task<IEnumerable<User>> FindAllAsync();
+    Task AddAsync(User user);
+    void Update(User user);
+}
+
