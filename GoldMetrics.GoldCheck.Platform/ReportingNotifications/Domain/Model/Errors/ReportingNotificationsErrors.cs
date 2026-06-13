be748a833ad426df8e45fd919a10bd84415afa6b
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:48:04 -0500

    feat(reportingnotifications): add base error enum and error catalog.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/Errors/ReportingNotificationsErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Errors;
+
+public static class ReportingNotificationsErrors
+{
+    public static ReportingNotificationsError None => ReportingNotificationsError.None;
+    public static ReportingNotificationsError ReportNotFound => ReportingNotificationsError.ReportNotFound;
+    public static ReportingNotificationsError AccidentValidationFailed => ReportingNotificationsError.AccidentValidationFailed;
+    public static ReportingNotificationsError OperationCancelled => ReportingNotificationsError.OperationCancelled;
+    public static ReportingNotificationsError DatabaseError => ReportingNotificationsError.DatabaseError;
+    public static ReportingNotificationsError InternalServerError => ReportingNotificationsError.InternalServerError;
+}
