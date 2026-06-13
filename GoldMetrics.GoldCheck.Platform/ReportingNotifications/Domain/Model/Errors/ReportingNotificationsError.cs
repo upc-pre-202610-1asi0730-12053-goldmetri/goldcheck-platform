70e5b5cbf7927c8c18c93f2cfe8fd9ddd2ba6b3c
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:46:57 -0500

    add reporting notifications error enum

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/Errors/ReportingNotificationsError.cs
@@ -0,0 +1,11 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Errors;
+
+public enum ReportingNotificationsError
+{
+    None,
+    ReportNotFound,
+    AccidentValidationFailed,
+    OperationCancelled,
+    DatabaseError,
+    InternalServerError
+}
