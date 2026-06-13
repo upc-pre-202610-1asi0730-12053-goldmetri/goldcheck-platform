7dd129ac08984693e91ba22ea6fdc28a00bd83e2
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:40:03 -0500

    feat(reportingnotifications): add supervisor id value object.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/ValueObjects/SupervisorId.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.ValueObjects;
+
+public record SupervisorId
+{
+    public SupervisorId() => Value = string.Empty;
+    public SupervisorId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("SupervisorId cannot be empty.", nameof(value));
+        Value = value;
+    }
+    public string Value { get; init; }
+}
