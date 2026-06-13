8a11111193f2d96db383e1d65355d3678aeaacf9
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:44:47 -0500

    feat(reportingnotifications): add accident data requested event.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/Events/AccidentDataRequestedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Events;
+
+public record AccidentDataRequestedEvent(string ReportId, string IncidentId, string SupervisorId) : IEvent;
