6d1aab22e24c41caafbba8b38a7d259755102d32
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:58:28 -0500

    feat(reportingnotifications): add report command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Application/CommandServices/IReportCommandService.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Application.CommandServices;
+
+public interface IReportCommandService
+{
+    Task<Result<Report>> Handle(RequestAccidentDataCommand command, CancellationToken cancellationToken);
+}
