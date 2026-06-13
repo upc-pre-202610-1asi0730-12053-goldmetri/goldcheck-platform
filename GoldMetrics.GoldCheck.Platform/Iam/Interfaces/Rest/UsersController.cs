c3cdd54a95990cbb21ed04cd84dd355cdf683943
kattyph <katty.philco17@gmail.com>
2026-06-13 03:13:45 -0500

    feat(iam): add update profile endpoint.

---
+++ GoldMetrics.GoldCheck.Platform/Iam/Interfaces/Rest/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using GoldMetrics.GoldCheck.Platform.Iam.Application.CommandServices;
+using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Iam.Interfaces.Rest.Resources;
+using GoldMetrics.GoldCheck.Platform.Iam.Interfaces.Rest.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Interfaces.Rest;
+
+[ApiController]
+[Route("api/v1/users")]
+[Produces("application/json")]
+public class UsersController(
+    IIamCommandService commandService,
+    IamActionResultAssembler assembler) : ControllerBase
+{
+    [HttpPut("{userId}/profile")]
+    [SwaggerOperation(Summary = "Update user profile", OperationId = "UpdateProfile")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateProfile([FromRoute] string userId, [FromBody] UpdateProfileResource resource, CancellationToken ct)
+    {
+        var command = new UpdateProfileCommand(userId, resource.Username, resource.Email);
+        var result = await commandService.UpdateProfileAsync(command, ct);
+        return assembler.ToActionResult(result, this);
+    }
+}
