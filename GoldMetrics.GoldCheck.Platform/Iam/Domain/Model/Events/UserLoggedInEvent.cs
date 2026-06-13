726c5858a9a772d0963dd69ae66acb43b035ee6e
kattyph <katty.philco17@gmail.com>
2026-06-12 19:04:50 -0500

    feat(iam): add iam domain events.

---
+++ GoldMetrics.GoldCheck.Platform/Iam/Domain/Model/Events/UserLoggedInEvent.cs
@@ -0,0 +1,6 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Events;
+
+public record UserLoggedInEvent(int UserId, string Username) : IEvent;
+
---
+++ GoldMetrics.GoldCheck.Platform/Iam/Domain/Model/Events/UserRegisteredEvent.cs
@@ -0,0 +1,6 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Events;
+
+public record UserRegisteredEvent(int UserId, string Username, string Email) : IEvent;
+
