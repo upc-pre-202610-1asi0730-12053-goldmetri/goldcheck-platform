21788f837ca43f39be2f675bcb8dde874b8fb761
kattyph <katty.philco17@gmail.com>
2026-06-12 20:08:57 -0500

    feat(iam): add iam command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/Iam/Application/CommandServices/IIamCommandService.cs
@@ -0,0 +1,10 @@
+using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Application.CommandServices;
+
+public interface IIamCommandService
+{
+    Task<Result<User>> RegisterUserAsync(RegisterUserCommand command, CancellationToken ct = default);
+}
