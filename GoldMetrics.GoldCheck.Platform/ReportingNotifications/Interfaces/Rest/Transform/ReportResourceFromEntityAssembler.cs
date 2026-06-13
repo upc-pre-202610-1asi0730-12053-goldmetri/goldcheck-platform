f697d05f53c74e728def4a87feba856dd127d33d
genixmvp <genix.mvp@gmail.com>
2026-06-13 10:13:34 -0500

    feat(reportingnotifications): add report resource assembler and action result assembler.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Interfaces/Rest/Transform/ReportResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Interfaces.Rest.Transform;
+
+public static class ReportResourceFromEntityAssembler
+{
+    public static ReportResource ToResourceFromEntity(Report entity) =>
+        new(entity.Id, entity.SupervisorId.Value, entity.IncidentId,
+            entity.ReportStatus.Value, entity.Status);
+}
