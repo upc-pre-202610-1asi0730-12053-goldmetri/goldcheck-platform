58e06a196625b46d091efc1bf0e10d16a34f8e6e
kattyph <katty.philco17@gmail.com>
2026-06-12 19:15:58 -0500

    feat(iam): add ef mapping for user.

---
+++ GoldMetrics.GoldCheck.Platform/Iam/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Aggregates;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldMetrics.GoldCheck.Platform.Iam.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder ApplyIAMConfiguration(this ModelBuilder builder)
+    {
+        builder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.OwnsOne(u => u.Username, vo => vo.Property(v => v.Value).HasColumnName("username").IsRequired());
+            entity.OwnsOne(u => u.HashedPassword, vo => vo.Property(v => v.Value).HasColumnName("hashed_password").IsRequired());
+            entity.OwnsOne(u => u.Email, vo => vo.Property(v => v.Address).HasColumnName("email").IsRequired());
+            entity.OwnsOne(u => u.Role, vo => vo.Property(v => v.Value).HasColumnName("role").IsRequired());
+            entity.Property(u => u.Status).IsRequired();
+        });
+        return builder;
+    }
+}
+
