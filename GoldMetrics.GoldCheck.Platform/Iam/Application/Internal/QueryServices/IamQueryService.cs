7e2ca489c6089da412cd18e05e2979c86419a7b1
kattyph <katty.philco17@gmail.com>
2026-06-13 03:27:50 -0500

    feat(iam): implement get user by id and get all users.

--- GoldMetrics.GoldCheck.Platform/Iam/Application/Internal/QueryServices/IamQueryService.cs
+++ GoldMetrics.GoldCheck.Platform/Iam/Application/Internal/QueryServices/IamQueryService.cs
@@ -1,5 +1,6 @@
 using GoldMetrics.GoldCheck.Platform.Iam.Application.QueryServices;
 using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Errors;
 using GoldMetrics.GoldCheck.Platform.Iam.Domain.Model.Queries;
 using GoldMetrics.GoldCheck.Platform.Iam.Domain.Repositories;
 using GoldMetrics.GoldCheck.Platform.Iam.Resources;
@@ -12,13 +13,42 @@ public class IamQueryService(
     IUserRepository userRepository,
     IStringLocalizer<IamMessages> localizer) : IIamQueryService
 {
-    public Task<Result<User>> GetUserByIdAsync(GetUserByIdQuery query, CancellationToken ct = default)
+    public async Task<Result<User>> GetUserByIdAsync(GetUserByIdQuery query, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (!int.TryParse(query.UserId, out var userId))
+                return Result<User>.Failure(IamError.UserNotFound, localizer[nameof(IamError.UserNotFound)]);
+
+            var user = await userRepository.FindByIdAsync(userId);
+            return user is null
+                ? Result<User>.Failure(IamError.UserNotFound, localizer[nameof(IamError.UserNotFound)])
+                : Result<User>.Success(user);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<User>.Failure(IamError.OperationCancelled, localizer[nameof(IamError.OperationCancelled)]);
+        }
+        catch (Exception)
+        {
+            return Result<User>.Failure(IamError.InternalServerError, localizer[nameof(IamError.InternalServerError)]);
+        }
     }
 
-    public Task<Result<IEnumerable<User>>> GetAllUsersAsync(GetAllUsersQuery query, CancellationToken ct = default)
+    public async Task<Result<IEnumerable<User>>> GetAllUsersAsync(GetAllUsersQuery query, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var users = await userRepository.FindAllAsync();
+            return Result<IEnumerable<User>>.Success(users);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<IEnumerable<User>>.Failure(IamError.OperationCancelled, localizer[nameof(IamError.OperationCancelled)]);
+        }
+        catch (Exception)
+        {
+            return Result<IEnumerable<User>>.Failure(IamError.InternalServerError, localizer[nameof(IamError.InternalServerError)]);
+        }
     }
 }
