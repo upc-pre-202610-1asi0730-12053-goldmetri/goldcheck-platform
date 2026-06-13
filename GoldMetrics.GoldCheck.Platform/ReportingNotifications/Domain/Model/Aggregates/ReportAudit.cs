c88c5bad27ef8d019c1da50f1f9f27e8f6232064
genixmvp <genix.mvp@gmail.com>
2026-06-13 09:44:02 -0500

    feat(reportingnotifications): add auditable fields to report aggregate.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/Aggregates/ReportAudit.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Entities;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+
+public partial class Report : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
