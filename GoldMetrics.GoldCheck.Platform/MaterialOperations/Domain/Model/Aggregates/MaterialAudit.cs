099e00be1b4c96ea687248bd80a7eda249e5e0be
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 16:41:41 -0500

    feat(materialoperations): add auditable fields to material aggregate.

---
+++ GoldMetrics.GoldCheck.Platform/MaterialOperations/Domain/Model/Aggregates/MaterialAudit.cs
@@ -0,0 +1,9 @@
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Entities;
+
+namespace GoldMetrics.GoldCheck.Platform.MaterialOperations.Domain.Model.Aggregates;
+
+public partial class Material : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
