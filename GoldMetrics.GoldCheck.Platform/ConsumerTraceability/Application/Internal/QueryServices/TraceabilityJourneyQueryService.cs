9e32c270e740fb88cf15aa1449fb903e7f585cc7
genixmvp <genix.mvp@gmail.com>
2026-06-14 23:15:32 -0500

    feat(consumertraceability): add get traceability journey query handling implementation.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Application/Internal/QueryServices/TraceabilityJourneyQueryService.cs
@@ -0,0 +1,15 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Application.QueryServices;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Queries;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Application.Internal.QueryServices;
+
+public class TraceabilityJourneyQueryService(ITraceabilityJourneyRepository journeyRepository)
+    : ITraceabilityJourneyQueryService
+{
+    public async Task<TraceabilityJourney?> Handle(
+        GetTraceabilityJourneyQuery query,
+        CancellationToken cancellationToken = default)
+        => await journeyRepository.FindLatestByQRCodeAsync(query.QRCode, cancellationToken);
+}
