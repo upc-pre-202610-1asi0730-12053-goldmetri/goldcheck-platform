0d1f5829771162a62328814820b96e1a960201c8
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 18:13:18 -0500

    feat(materialoperations): add material already classified error.

--- GoldMetrics.GoldCheck.Platform/MaterialOperations/Domain/Model/MaterialOperationsError.cs
+++ GoldMetrics.GoldCheck.Platform/MaterialOperations/Domain/Model/MaterialOperationsError.cs
@@ -6,6 +6,7 @@ public enum MaterialOperationsError
     MaterialNotFound,
     InvalidMineralType,
     InvalidPayload,
+    MaterialAlreadyClassified,
     OperationCancelled,
     DatabaseError,
     InternalServerError
