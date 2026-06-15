68adfdd99996a9412d0710a935caae9dc3f55778
kattyph <katty.philco17@gmail.com>
2026-06-14 23:24:08 -0500

    feat(reporting-notifications): add get report by id query handling implementation.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Application/Internal/QueryServices/ReportQueryService.cs
@@ -0,0 +1,12 @@
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Application.QueryServices;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Queries;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Application.Internal.QueryServices;
+
+public class ReportQueryService(IReportRepository reportRepository) : IReportQueryService
+{
+    public async Task<Report?> Handle(GetReportByIdQuery query, CancellationToken cancellationToken)
+        => await reportRepository.FindByIdAsync(query.Id, cancellationToken);
+}
