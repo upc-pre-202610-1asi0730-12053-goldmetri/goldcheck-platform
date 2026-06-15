7c52863610d1850c4c49350dc5e7a29a01787460
kattyph <katty.philco17@gmail.com>
2026-06-15 00:33:18 -0500

    feat(reporting-notifications): add notification command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Application/CommandServices/INotificationCommandService.cs
@@ -0,0 +1,10 @@
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Application.CommandServices;
+
+public interface INotificationCommandService
+{
+    Task<Result<Notification>> Handle(RequestNotificationCommand command, CancellationToken cancellationToken);
+}
