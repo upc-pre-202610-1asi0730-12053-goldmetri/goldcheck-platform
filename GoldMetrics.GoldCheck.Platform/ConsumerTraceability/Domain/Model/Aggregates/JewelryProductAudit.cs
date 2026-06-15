c8aee68d1c74d8db2f7d57e6135b94bc0d137245
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:12:03 -0500

    feat(consumertraceability): add auditable fields to jewelry product aggregate.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Domain/Model/Aggregates/JewelryProductAudit.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Entities;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+
+public partial class JewelryProduct : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
