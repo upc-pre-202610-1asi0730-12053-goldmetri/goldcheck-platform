5c6ab66a9ef468535d00227de1bea1c18ccc790b
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:15:20 -0500

    feat(consumertraceability): add base error enum and error catalog.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Domain/Exceptions/ConsumerTraceabilityError.cs
@@ -0,0 +1,11 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Exceptions;
+
+public enum ConsumerTraceabilityError
+{
+    None,
+    ProductNotFound,
+    InvalidQRCode,
+    OperationCancelled,
+    DatabaseError,
+    InternalServerError
+}
---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Domain/Exceptions/ConsumerTraceabilityErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Exceptions;
+
+public static class ConsumerTraceabilityErrors
+{
+    public static ConsumerTraceabilityError None => ConsumerTraceabilityError.None;
+    public static ConsumerTraceabilityError ProductNotFound => ConsumerTraceabilityError.ProductNotFound;
+    public static ConsumerTraceabilityError InvalidQRCode => ConsumerTraceabilityError.InvalidQRCode;
+    public static ConsumerTraceabilityError OperationCancelled => ConsumerTraceabilityError.OperationCancelled;
+    public static ConsumerTraceabilityError DatabaseError => ConsumerTraceabilityError.DatabaseError;
+    public static ConsumerTraceabilityError InternalServerError => ConsumerTraceabilityError.InternalServerError;
+}
