f60165157704203ad0621e39c8c75ff81e2bbe31
genixmvp <genix.mvp@gmail.com>
2026-06-14 23:09:02 -0500

    feat(consumertraceability): add traceability journey command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Application/CommandServices/ITraceabilityJourneyCommandService.cs
@@ -0,0 +1,11 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Application.CommandServices;
+
+public interface ITraceabilityJourneyCommandService
+{
+    Task<Result<TraceabilityJourney>> Handle(RequestJourneyCommand command,
+        CancellationToken cancellationToken = default);
+}
