99ab248b550da09da04b013a45ea93af210bddfa
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 17:19:30 -0500

    feat(materialoperations): add get material by id query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/MaterialOperations/Application/QueryServices/IMaterialQueryService.cs
@@ -0,0 +1,9 @@
+using GoldMetrics.GoldCheck.Platform.MaterialOperations.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MaterialOperations.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.MaterialOperations.Application.QueryServices;
+
+public interface IMaterialQueryService
+{
+    Task<Material?> Handle(GetMaterialByIdQuery query, CancellationToken cancellationToken);
+}
