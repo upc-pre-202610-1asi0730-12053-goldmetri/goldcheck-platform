6a99c81a0bb12f131ca5e53a43d1935537b4d7ea
genixmvp <genix.mvp@gmail.com>
2026-06-14 22:46:23 -0500

    feat(consumertraceability): add get product by qr query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Application/QueryServices/IJewelryProductQueryService.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Application.QueryServices;
+
+public interface IJewelryProductQueryService
+{
+    Task<JewelryProduct?> Handle(GetProductByQRQuery query,
+        CancellationToken cancellationToken = default);
+}
