f5fbadcf29ac735768f5db73b682ba4a6e9b6555
kattyph <katty.philco17@gmail.com>
2026-06-15 02:16:18 -0500

    feat(reporting-notifications): add get notification by id query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Application/QueryServices/INotificationQueryService.cs
@@ -0,0 +1,9 @@
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Application.QueryServices;
+
+public interface INotificationQueryService
+{
+    Task<Notification?> Handle(GetNotificationByIdQuery query, CancellationToken cancellationToken);
+}
