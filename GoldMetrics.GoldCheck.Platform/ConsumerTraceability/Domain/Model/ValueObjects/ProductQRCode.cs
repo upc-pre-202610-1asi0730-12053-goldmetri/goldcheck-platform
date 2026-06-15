ef83102d2d3d7fa24d6ff22f5bda6e2d687ab036
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:10:10 -0500

    feat(consumertraceability): add product qr code value object.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Domain/Model/ValueObjects/ProductQRCode.cs
@@ -0,0 +1,15 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.ValueObjects;
+
+public record ProductQRCode
+{
+    public ProductQRCode() => Value = string.Empty;
+
+    public ProductQRCode(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("ProductQRCode cannot be empty.", nameof(value));
+        Value = value;
+    }
+
+    public string Value { get; init; }
+}
