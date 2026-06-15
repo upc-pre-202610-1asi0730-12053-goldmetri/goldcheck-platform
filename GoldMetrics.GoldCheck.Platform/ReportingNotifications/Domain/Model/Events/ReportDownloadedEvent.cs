078041b24fd7b91cbc60dc2f62e61d762de7a689
kattyph <katty.philco17@gmail.com>
2026-06-14 23:04:12 -0500

    feat(reporting-notifications): add report downloaded event.

---
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/Events/ReportDownloadedEvent.cs
@@ -0,0 +1,5 @@
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.ReportingNotifications.Domain.Model.Events;
+
+public record ReportDownloadedEvent(string ReportId, string UserId) : IEvent;
