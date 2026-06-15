6a7022ebbee93858dc59f55918349e835b9ad066
kattyph <katty.philco17@gmail.com>
2026-06-14 23:02:30 -0500

    feat(reporting-notifications): update report status value object to allow downloaded state.

--- GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/ValueObjects/ReportStatus.cs
+++ GoldMetrics.GoldCheck.Platform/ReportingNotifications/Domain/Model/ValueObjects/ReportStatus.cs
@@ -2,7 +2,7 @@
 
 public record ReportStatus
 {
-    private static readonly string[] AllowedValues = ["Requested", "DataLoaded", "Generated", "ExportationRequested", "Exported"];
+    private static readonly string[] AllowedValues = ["Requested", "DataLoaded", "Generated", "ExportationRequested", "Exported", "Downloaded"];
     public ReportStatus() => Value = string.Empty;
     public ReportStatus(string value)
     {
