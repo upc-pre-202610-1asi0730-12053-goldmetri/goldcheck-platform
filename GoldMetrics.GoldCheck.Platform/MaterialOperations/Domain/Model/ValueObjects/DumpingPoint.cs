45b0064c801683bac9ac2a8c3c06a13e07fa0aa1
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 18:22:03 -0500

    feat(materialoperations): add dumping point value object.

---
+++ GoldMetrics.GoldCheck.Platform/MaterialOperations/Domain/Model/ValueObjects/DumpingPoint.cs
@@ -0,0 +1,15 @@
+namespace GoldMetrics.GoldCheck.Platform.MaterialOperations.Domain.Model.ValueObjects;
+
+public record DumpingPoint
+{
+    public DumpingPoint() => Name = string.Empty;
+
+    public DumpingPoint(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("DumpingPoint name cannot be empty.", nameof(name));
+        Name = name;
+    }
+
+    public string Name { get; init; }
+}
