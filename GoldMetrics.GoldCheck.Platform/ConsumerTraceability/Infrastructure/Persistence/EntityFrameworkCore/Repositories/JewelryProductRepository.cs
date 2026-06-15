bea60e3273d32ce88e9204d17f88368331170f78
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:28:29 -0500

    feat(consumertraceability): add jewelry product repository implementation.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Infrastructure/Persistence/EntityFrameworkCore/Repositories/JewelryProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Repositories;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class JewelryProductRepository(AppDbContext context)
+    : BaseRepository<JewelryProduct>(context), IJewelryProductRepository
+{
+    public async Task<JewelryProduct?> FindByQRCodeAsync(
+        string qrCode,
+        CancellationToken cancellationToken = default)
+        => await Context.Set<JewelryProduct>()
+            .FirstOrDefaultAsync(p => p.QRCode.Value == qrCode, cancellationToken);
+}
