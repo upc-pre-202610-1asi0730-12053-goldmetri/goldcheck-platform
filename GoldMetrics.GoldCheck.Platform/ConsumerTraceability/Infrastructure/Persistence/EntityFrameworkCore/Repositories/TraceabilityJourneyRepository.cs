d6d88f3bf1b3e0c4a9a758b288bfbed58a38c846
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:29:01 -0500

    feat(consumertraceability): add traceability journey repository implementation.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Infrastructure/Persistence/EntityFrameworkCore/Repositories/TraceabilityJourneyRepository.cs
@@ -0,0 +1,11 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Repositories;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class TraceabilityJourneyRepository(AppDbContext context)
+    : BaseRepository<TraceabilityJourney>(context), ITraceabilityJourneyRepository
+{
+}
