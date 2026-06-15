ae36d99e83bb078aae8ce23bb2d629b79f812f21
genixmvp <genix.mvp@gmail.com>
2026-06-14 22:08:00 -0500

    feat(consumertraceability): add scan product qr resources.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Interfaces/Rest/Resources/JewelryProductResource.cs
@@ -0,0 +1,8 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Interfaces.Rest.Resources;
+
+public record JewelryProductResource(
+    int Id,
+    string QRCode,
+    string ConsumerId,
+    string Status,
+    int ScanCount);
