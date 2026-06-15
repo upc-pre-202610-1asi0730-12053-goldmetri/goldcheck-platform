414e01f8d70bbe5896e718742c95f62e01adfd8a
genixmvp <genix.mvp@gmail.com>
2026-06-14 21:13:43 -0500

    feat(consumertraceability): add product qr scanned event.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Domain/Model/Events/ProductQRScannedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Events;
+
+public record ProductQRScannedEvent(string QRCode, string ConsumerId) : IEvent;
