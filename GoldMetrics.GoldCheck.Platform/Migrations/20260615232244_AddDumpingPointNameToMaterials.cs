ee9382a48858dd586501990d4f29ada7cf9afb73
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 18:23:24 -0500

    feat(materialoperations): add persistence mapping for download material.

---
+++ GoldMetrics.GoldCheck.Platform/Migrations/20260615232244_AddDumpingPointNameToMaterials.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoldMetrics.GoldCheck.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDumpingPointNameToMaterials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "dumping_point_name",
+                table: "materials",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dumping_point_name",
+                table: "materials");
+        }
+    }
+}
