1cff638afcc79170e4d2386e936c54e0e5f9d29e
genixmvp <genix.mvp@gmail.com>
2026-06-14 23:17:31 -0500

    feat(consumertraceability): add get traceability journey resource and assembler.

---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Interfaces/Rest/Resources/TraceabilityJourneyResource.cs
@@ -0,0 +1,8 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Interfaces.Rest.Resources;
+
+public record TraceabilityJourneyResource(
+    int Id,
+    string ProductQRCode,
+    string ConsumerId,
+    string JourneySummary,
+    string Status);
---
+++ GoldMetrics.GoldCheck.Platform/ConsumerTraceability/Interfaces/Rest/Transform/TraceabilityJourneyResourceFromEntityAssembler.cs
@@ -0,0 +1,15 @@
+﻿using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.ConsumerTraceability.Interfaces.Rest.Transform;
+
+public static class TraceabilityJourneyResourceFromEntityAssembler
+{
+    public static TraceabilityJourneyResource ToResourceFromEntity(TraceabilityJourney entity)
+        => new(
+            entity.Id,
+            entity.ProductQRCode.Value,
+            entity.ConsumerId.Value,
+            entity.JourneySummary,
+            entity.Status);
+}
