f23c92afa225bc1e03530276967de558abac6f2a
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 20:10:51 -0500

    feat(jewelryinventory): add jewelry material repository contract.

---
+++ GoldMetrics.GoldCheck.Platform/JewelryInventory/Domain/Repositories/IJewelryMaterialRepository.cs
@@ -0,0 +1,10 @@
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Repositories;
+
+public interface IJewelryMaterialRepository : IBaseRepository<JewelryMaterial>
+{
+    Task<JewelryMaterial?> FindByMaterialIdAsync(string materialId,
+        CancellationToken cancellationToken = default);
+}
