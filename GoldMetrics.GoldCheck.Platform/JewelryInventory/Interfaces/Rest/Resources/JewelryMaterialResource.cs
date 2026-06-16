b99b12dc9ba0c573abe46ff446cb3c9bd9de4a4e
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 20:12:28 -0500

    feat(jewelryinventory): add register non certified material resources.

---
+++ GoldMetrics.GoldCheck.Platform/JewelryInventory/Interfaces/Rest/Resources/JewelryMaterialResource.cs
@@ -0,0 +1,7 @@
+namespace GoldMetrics.GoldCheck.Platform.JewelryInventory.Interfaces.Rest.Resources;
+
+public record JewelryMaterialResource(
+    int Id,
+    string MaterialId,
+    string JewelerId,
+    string Status);
