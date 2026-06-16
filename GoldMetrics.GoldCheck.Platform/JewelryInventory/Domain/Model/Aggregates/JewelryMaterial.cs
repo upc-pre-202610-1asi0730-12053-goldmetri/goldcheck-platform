abc2837e7e543d1c7f0a9ea42f204bf8d7b12c86
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 20:10:24 -0500

    feat(jewelryinventory): add jewelry material aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/JewelryInventory/Domain/Model/Aggregates/JewelryMaterial.cs
@@ -0,0 +1,26 @@
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.Aggregates;
+
+public partial class JewelryMaterial
+{
+    public JewelryMaterial()
+    {
+        MaterialId = new MaterialId();
+        JewelerId = new JewelerId();
+        Status = new MaterialStatus();
+    }
+
+    public JewelryMaterial(RegisterNonCertifiedMaterialCommand command)
+    {
+        MaterialId = new MaterialId(command.MaterialId);
+        JewelerId = new JewelerId(command.JewelerId);
+        Status = new MaterialStatus("NonCertified");
+    }
+
+    public int Id { get; }
+    public MaterialId MaterialId { get; private set; }
+    public JewelerId JewelerId { get; private set; }
+    public MaterialStatus Status { get; private set; }
+}
