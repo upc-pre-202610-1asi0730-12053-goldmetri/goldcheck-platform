a7d6e29363c249e511151ea370683d2be8743f64
vicmacode <victorgarciap10@hotmail.com>
2026-06-15 20:12:33 -0500

    feat(jewelryinventory): add jewelry material resource assembler and action result assembler.

---
+++ GoldMetrics.GoldCheck.Platform/JewelryInventory/Interfaces/Rest/Transform/JewelryInventoryActionResultAssembler.cs
@@ -0,0 +1,37 @@
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Exceptions;
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+using GoldMetrics.GoldCheck.Platform.Shared.Resources.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using ProblemDetailsFactory =
+    GoldMetrics.GoldCheck.Platform.Shared.Interfaces.Rest.ProblemDetails.ProblemDetailsFactory;
+
+namespace GoldMetrics.GoldCheck.Platform.JewelryInventory.Interfaces.Rest.Transform;
+
+public static class JewelryInventoryActionResultAssembler
+{
+    private static int ToStatusCode(JewelryInventoryError error) => error switch
+    {
+        JewelryInventoryError.MaterialNotFound         => StatusCodes.Status404NotFound,
+        JewelryInventoryError.MaterialAlreadyCertified => StatusCodes.Status409Conflict,
+        JewelryInventoryError.OperationCancelled       => StatusCodes.Status409Conflict,
+        JewelryInventoryError.DatabaseError            => StatusCodes.Status500InternalServerError,
+        _                                              => StatusCodes.Status500InternalServerError
+    };
+
+    public static IActionResult ToActionResultFromMaterialResult(
+        ControllerBase controller,
+        Result<JewelryMaterial> result,
+        IStringLocalizer<ErrorMessages> localizer,
+        ProblemDetailsFactory problemDetailsFactory,
+        Func<JewelryMaterial, IActionResult> onSuccess)
+    {
+        if (result.IsSuccess) return onSuccess(result.Value!);
+
+        var statusCode = ToStatusCode((JewelryInventoryError)result.Error!);
+        return problemDetailsFactory.CreateProblemDetails(
+            controller, statusCode, result.Error, result.Message);
+    }
+}
---
+++ GoldMetrics.GoldCheck.Platform/JewelryInventory/Interfaces/Rest/Transform/JewelryMaterialResourceFromEntityAssembler.cs
@@ -0,0 +1,14 @@
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.JewelryInventory.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.JewelryInventory.Interfaces.Rest.Transform;
+
+public static class JewelryMaterialResourceFromEntityAssembler
+{
+    public static JewelryMaterialResource ToResourceFromEntity(JewelryMaterial entity)
+        => new(
+            entity.Id,
+            entity.MaterialId.Value,
+            entity.JewelerId.Value,
+            entity.Status.Value);
+}
