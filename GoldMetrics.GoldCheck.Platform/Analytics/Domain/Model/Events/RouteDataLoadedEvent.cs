0915ffaffb70e473685e68232973d26dea111c7c
genixmvp <genix.mvp@gmail.com>
2026-06-17 00:48:58 -0500

    feat(analytics): add route data loaded event.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/Events/RouteDataLoadedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Events;
+
+public record RouteDataLoadedEvent(string RouteId, string UserId) : IEvent;
