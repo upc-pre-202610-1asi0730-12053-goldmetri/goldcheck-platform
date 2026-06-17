03f98545ce2b1035d86383a6823e1af2818d7e6a
genixmvp <genix.mvp@gmail.com>
2026-06-17 00:56:39 -0500

    feat(analytics): add analytics command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Application/CommandServices/IAnalyticsCommandService.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Application.CommandServices;
+
+public interface IAnalyticsCommandService
+{
+    Task<Result<Material>> Handle(ViewRouteProgressCommand command, CancellationToken cancellationToken);
+}
