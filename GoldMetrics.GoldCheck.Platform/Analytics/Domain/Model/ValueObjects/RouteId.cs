fbfaf8464f46ad4f5bf67fa91dd2db4b7955800e
genixmvp <genix.mvp@gmail.com>
2026-06-17 00:37:25 -0500

    feat(analytics): add route id value object.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/ValueObjects/RouteId.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.ValueObjects;
+
+public record RouteId
+{
+    public RouteId() => Value = string.Empty;
+    public RouteId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("RouteId cannot be empty.", nameof(value));
+        Value = value;
+    }
+    public string Value { get; init; }
+}
