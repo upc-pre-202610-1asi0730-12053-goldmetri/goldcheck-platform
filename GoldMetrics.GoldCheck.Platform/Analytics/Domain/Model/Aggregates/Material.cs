913f455aa706964b1f37e3b88e5717d65ec3656e
genixmvp <genix.mvp@gmail.com>
2026-06-17 00:47:37 -0500

    feat(analytics): add material aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/Aggregates/Material.cs
@@ -0,0 +1,35 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Aggregates;
+
+public partial class Material
+{
+    public Material()
+    {
+        MaterialId = new MaterialId();
+        RouteId = new RouteId();
+        RouteStatus = new RouteStatus();
+        SupervisorId = new SupervisorId();
+        UserId = new UserId();
+        Status = string.Empty;
+    }
+
+    public Material(ViewRouteProgressCommand command)
+    {
+        MaterialId = new MaterialId(Guid.NewGuid().ToString());
+        RouteId = new RouteId(command.RouteId);
+        UserId = new UserId(command.UserId);
+        SupervisorId = new SupervisorId();
+        RouteStatus = new RouteStatus("InProgress");
+        Status = "RouteDataLoaded";
+    }
+
+    public int Id { get; }
+    public MaterialId MaterialId { get; private set; }
+    public RouteId RouteId { get; private set; }
+    public RouteStatus RouteStatus { get; private set; }
+    public SupervisorId SupervisorId { get; private set; }
+    public UserId UserId { get; private set; }
+    public string Status { get; private set; }
+}
