1caac593ef7e336e0a0bb72385367d3f365fe899
genixmvp <genix.mvp@gmail.com>
2026-06-17 00:54:55 -0500

    feat(analytics): add material repository contract.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Repositories/IMaterialRepository.cs
@@ -0,0 +1,8 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Repositories;
+
+public interface IMaterialRepository : IBaseRepository<Material>
+{
+}
