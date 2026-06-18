7e221d05f7f7fc5e102a4f7add183ead82039424
genixmvp <genix.mvp@gmail.com>
2026-06-18 02:28:18 -0500

    feat(analytics): add production volume value object.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/ValueObjects/ProductionVolume.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.ValueObjects;
+
+public record ProductionVolume
+{
+    public ProductionVolume() => Tons = 0;
+    public ProductionVolume(decimal tons)
+    {
+        if (tons < 0)
+            throw new ArgumentException("ProductionVolume cannot be negative.", nameof(tons));
+        Tons = tons;
+    }
+    public decimal Tons { get; init; }
+}
