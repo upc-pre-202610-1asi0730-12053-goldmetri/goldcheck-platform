76a671ef793e260507884b08867d7f4ecbb8832e
genixmvp <genix.mvp@gmail.com>
2026-06-18 01:27:42 -0500

    feat(analytics): add production data requested event.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/Events/ProductionDataRequestedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Events;
+
+public record ProductionDataRequestedEvent(string SupervisorId, DateTimeOffset Start, DateTimeOffset End) : IEvent;
