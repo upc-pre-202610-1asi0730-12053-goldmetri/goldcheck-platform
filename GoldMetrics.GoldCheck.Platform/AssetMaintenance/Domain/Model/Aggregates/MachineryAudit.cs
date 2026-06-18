700ae02ba1b8d40e4f2911bf11a70a0bf133f5bc
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 16:29:37 -0500

    feat(assetmaintenance): add auditable fields to machinery aggregate.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Domain/Model/Aggregates/MachineryAudit.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Entities;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Aggregates;
+
+public partial class Machinery : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
