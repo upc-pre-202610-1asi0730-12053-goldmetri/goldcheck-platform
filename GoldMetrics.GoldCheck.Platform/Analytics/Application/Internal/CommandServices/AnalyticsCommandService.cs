a744db55193bf7ad8933c4e20923cfc81bb7060e
genixmvp <genix.mvp@gmail.com>
2026-06-17 19:30:38 -0500

    feature(docs): add consumer traceability domain class diagram.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Application/Internal/CommandServices/AnalyticsCommandService.cs
@@ -0,0 +1,12 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Commands;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Application.Internal.CommandServices;
+
+public partial class AnalyticsCommandService
+{
+    
+    
+    //Paso 16 de la guia de analytics que no entendi
+    partial void Handle(ViewRouteProgressCommand command, CancellationToken cancellationToken);
+
+}
