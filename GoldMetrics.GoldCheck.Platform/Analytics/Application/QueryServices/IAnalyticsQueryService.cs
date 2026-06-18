4306584598d62a26530c2704cd73806e8e6f02c1
genixmvp <genix.mvp@gmail.com>
2026-06-18 00:06:17 -0500

    feat(analytics): add analytics query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Application/QueryServices/IAnalyticsQueryService.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Application.QueryServices;
+
+public interface IAnalyticsQueryService
+{
+    Task<Material?> Handle(GetRouteProgressByIdQuery query, CancellationToken cancellationToken);
+}
