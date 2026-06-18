42364664686f500cd99c1e4e13be27557b731c01
genixmvp <genix.mvp@gmail.com>
2026-06-18 02:29:26 -0500

    feat(analytics): add production data validated event.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/Events/ProductionDataValidatedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Events;
+
+public record ProductionDataValidatedEvent(string MaterialId) : IEvent;
