879fc8107fd6aa639a97b23309643577ab1a09c1
genixmvp <genix.mvp@gmail.com>
2026-06-18 01:21:49 -0500

    feat(analytics): add request production data command.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/Commands/RequestProductionDataCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Commands;
+
+public record RequestProductionDataCommand(string SupervisorId, DateTimeOffset Start, DateTimeOffset End);
