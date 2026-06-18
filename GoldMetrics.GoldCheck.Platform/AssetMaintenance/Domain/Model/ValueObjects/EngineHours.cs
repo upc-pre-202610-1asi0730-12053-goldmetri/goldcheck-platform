b961a70be9ca852f9875d5e44a38c41b4c1c2e54
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 16:27:05 -0500

    feat(assetmaintenance): add engine hours value object.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Domain/Model/ValueObjects/EngineHours.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.ValueObjects;
+
+public record EngineHours
+{
+    public EngineHours() => Hours = 0;
+    public EngineHours(decimal hours)
+    {
+        if (hours < 0)
+            throw new ArgumentException("EngineHours cannot be negative.", nameof(hours));
+        Hours = hours;
+    }
+    public decimal Hours { get; init; }
+}
