8ca8a73aef99dd5903581b17384840c428b3c0a2
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 16:43:47 -0500

    feat(assetmaintenance): add machinery repository contract.feat(assetmaintenance): add machinery registered messages.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Domain/Repositories/IMachineryRepository.cs
@@ -0,0 +1,8 @@
+﻿using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Repositories;
+
+public interface IMachineryRepository : IBaseRepository<Machinery>
+{
+}
