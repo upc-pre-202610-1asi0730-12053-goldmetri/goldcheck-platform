2d99b890c9f462cea84e53fe378485ac523a8c11
genixmvp <genix.mvp@gmail.com>
2026-06-18 01:23:55 -0500

    feat(analytics): add production period value object.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Domain/Model/ValueObjects/ProductionPeriod.cs
@@ -0,0 +1,15 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.ValueObjects;
+
+public record ProductionPeriod
+{
+    public ProductionPeriod() { Start = DateTimeOffset.MinValue; End = DateTimeOffset.MinValue; }
+    public ProductionPeriod(DateTimeOffset start, DateTimeOffset end)
+    {
+        if (start >= end)
+            throw new ArgumentException("Production period Start must be earlier than End.");
+        Start = start;
+        End = end;
+    }
+    public DateTimeOffset Start { get; init; }
+    public DateTimeOffset End { get; init; }
+}
