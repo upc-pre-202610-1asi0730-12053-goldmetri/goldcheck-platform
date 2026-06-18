7d8f44a0e0c7678947115c544c609d01c4218fe6
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 18:47:22 -0500

    feat(assetmaintenance): add machinery discharged event.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Domain/Model/Events/MachineryDischargedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Events;
+
+public record MachineryDischargedEvent(string MachineryId, string Reason) : IEvent;
