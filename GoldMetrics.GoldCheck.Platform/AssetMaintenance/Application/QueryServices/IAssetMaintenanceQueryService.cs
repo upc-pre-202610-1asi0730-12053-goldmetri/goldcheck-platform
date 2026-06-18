80298b3283c97bb66622067a288d3699deda2d90
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 17:15:10 -0500

    feat(assetmaintenance): add asset maintenance query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Application/QueryServices/IAssetMaintenanceQueryService.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Application.QueryServices;
+
+public interface IAssetMaintenanceQueryService
+{
+    Task<Machinery?> Handle(GetMachineryByIdQuery query, CancellationToken cancellationToken);
+}
