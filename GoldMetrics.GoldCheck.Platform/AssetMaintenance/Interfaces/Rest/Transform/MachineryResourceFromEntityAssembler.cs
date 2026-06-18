4e3c29fa4dfb1d5ba0735e55593f226ac63c2c2a
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 17:04:53 -0500

    feat(assetmaintenance): add machinery resource assembler and action result assembler.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Interfaces/Rest/Transform/MachineryResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Interfaces.Rest.Transform;
+
+public static class MachineryResourceFromEntityAssembler
+{
+    public static MachineryResource ToResourceFromEntity(Machinery entity) =>
+        new(entity.Id, entity.MachineryId.Value, entity.Model, entity.OEM,
+            entity.EngineHours.Hours, entity.MaintenanceStatus.Value, entity.Status);
+}
