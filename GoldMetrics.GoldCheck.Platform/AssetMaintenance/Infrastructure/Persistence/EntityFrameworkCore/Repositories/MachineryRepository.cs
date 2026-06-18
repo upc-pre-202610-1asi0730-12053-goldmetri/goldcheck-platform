f0d0a180bdc4840c3541360bab5b86e85160ffb3
Adrian5102 <u202410084@upc.edu.pe>
2026-06-18 16:45:44 -0500

    feat(assetmaintenance): add machinery repository implementation.

---
+++ GoldMetrics.GoldCheck.Platform/AssetMaintenance/Infrastructure/Persistence/EntityFrameworkCore/Repositories/MachineryRepository.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.AssetMaintenance.Domain.Repositories;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.AssetMaintenance.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class MachineryRepository(AppDbContext context) : BaseRepository<Machinery>(context), IMachineryRepository
+{
+}
