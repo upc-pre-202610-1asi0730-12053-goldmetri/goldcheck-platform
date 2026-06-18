f5d2a9c2960c21b4933b1af369383480a6801aa3
genixmvp <genix.mvp@gmail.com>
2026-06-17 23:10:52 -0500

    feat(analytics): add material repository implementation.

---
+++ GoldMetrics.GoldCheck.Platform/Analytics/Infrastructure/Persistence/EntityFrameworkCore/Repositories/MaterialRepository.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Analytics.Domain.Repositories;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using GoldMetrics.GoldCheck.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.Analytics.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class MaterialRepository(AppDbContext context) : BaseRepository<Material>(context), IMaterialRepository
+{
+}
