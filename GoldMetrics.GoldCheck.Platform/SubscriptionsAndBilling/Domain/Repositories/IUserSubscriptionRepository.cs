9e2f1549e5ec97720dc30f5221e780c92273f242
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 14:19:37 -0500

    feat(subscriptionsandbilling): add user subscription repository contract.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Repositories/IUserSubscriptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Repositories;
+
+public interface IUserSubscriptionRepository : IBaseRepository<UserSubscription>
+{
+    Task<UserSubscription?> FindByUserIdAsync(string userId, CancellationToken ct = default);
+}
