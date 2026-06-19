087f6c15be9c2f27a05914698deca9e4d657685b
vicmacode <victorgarciap10@hotmail.com>
2026-06-19 01:48:53 -0500

    feat(fleetoperations): add hauling cycle repository contract.

---
+++ GoldMetrics.GoldCheck.Platform/FleetOperations/Domain/Repositories/IHaulingCycleRepository.cs
@@ -0,0 +1,8 @@
+using GoldMetrics.GoldCheck.Platform.FleetOperations.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.Shared.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.FleetOperations.Domain.Repositories;
+
+public interface IHaulingCycleRepository : IBaseRepository<HaulingCycle>
+{
+}
