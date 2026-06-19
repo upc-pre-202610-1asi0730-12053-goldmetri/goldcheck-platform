5e9de340717d257c02ce2cf10d7c89a16e1b1332
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 18:31:50 -0500

    feat(subscriptionsandbilling): add access requested event.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/Events/AccessRequestedEvent.cs
@@ -0,0 +1,3 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Events;
+public record AccessRequestedEvent(string UserId, string FeatureName) : IEvent;
