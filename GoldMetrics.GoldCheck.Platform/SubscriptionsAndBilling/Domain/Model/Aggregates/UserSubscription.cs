01fafbf9acf7833c5feaeb24afc6ce789d89f8a4
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 13:56:57 -0500

    feat(subscriptionsandbilling): add user subscription aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/Aggregates/UserSubscription.cs
@@ -0,0 +1,32 @@
+﻿using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Entities;
+using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Aggregates;
+
+public partial class UserSubscription
+{
+    public int Id { get; private set; }
+    public UserId UserId { get; private set; } = new();
+    public PlanType PlanType { get; private set; } = new();
+    public BillingCycle BillingCycle { get; private set; } = new();
+    public SubscriptionStatus SubscriptionStatus { get; private set; } = new();
+    public List<string> AssignedFeatures { get; private set; } = [];
+    public List<Invoice> Invoices { get; private set; } = [];
+    public bool AccessGranted { get; private set; }
+    public string? RequestedDowngradePlan { get; private set; }
+    public string Status { get; private set; } = string.Empty;
+
+    public UserSubscription() { }
+
+    public UserSubscription(SelectPlanCommand command)
+    {
+        UserId = new UserId(command.UserId);
+        PlanType = new PlanType(command.PlanType);
+        BillingCycle = new BillingCycle(command.BillingCycle);
+        SubscriptionStatus = new SubscriptionStatus("Active");
+        AssignedFeatures = [];
+        Invoices = [];
+        Status = "PlanSelected";
+    }
+}
