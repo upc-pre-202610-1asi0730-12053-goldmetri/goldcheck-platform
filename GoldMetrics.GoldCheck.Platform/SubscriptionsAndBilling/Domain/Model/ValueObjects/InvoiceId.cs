87181983eea2692f368642782b51ba9314eb9925
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 13:57:28 -0500

    feat(subscriptionsandbilling): add invoice id value object.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/ValueObjects/InvoiceId.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.ValueObjects;
+public record InvoiceId
+{
+    public InvoiceId() => Value = string.Empty;
+    public InvoiceId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("InvoiceId cannot be empty.", nameof(value));
+        Value = value;
+    }
+    public string Value { get; init; }
+}
