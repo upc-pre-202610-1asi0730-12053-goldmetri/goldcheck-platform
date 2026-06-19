7c90331d022f134db4fe4e9d58d1a9c9331accea
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 14:01:44 -0500

    feat(subscriptionsandbilling): add auditable fields to user subscription aggregate.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/Aggregates/UserSubscriptionAudit.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Entities;
+
+namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Aggregates;
+
+public partial class UserSubscription : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
