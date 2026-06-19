65d9d5bc8044763f78bcc79f6c671d45ba27c0ec
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 15:11:27 -0500

    feat(subscriptionsandbilling): add select plan and base resources.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Interfaces/Rest/Resources/InvoiceResponse.cs
@@ -0,0 +1,2 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Interfaces.Rest.Resources;
+public record InvoiceResponse(int Id, string InvoiceId, decimal Amount, string Status);
---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Interfaces/Rest/Resources/UserSubscriptionResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Interfaces.Rest.Resources;
+public record UserSubscriptionResponse(int Id, string UserId, string PlanType, string BillingCycle,
+    string SubscriptionStatus, List<string> AssignedFeatures, bool AccessGranted, string Status);
