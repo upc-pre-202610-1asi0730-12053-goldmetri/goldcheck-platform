ea8d7cba5a3046c0d9d09a0d34415ebe6b0e23fa
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 13:46:48 -0500

    feat(subscriptionsandbilling): add user id value object.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/ValueObjects/UserId.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.ValueObjects;
+public record UserId
+{
+    public UserId() => Value = string.Empty;
+    public UserId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("UserId cannot be empty.", nameof(value));
+        Value = value;
+    }
+    public string Value { get; init; }
+}
