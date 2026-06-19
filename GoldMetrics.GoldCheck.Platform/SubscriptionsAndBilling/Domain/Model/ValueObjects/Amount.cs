9aa97ab95f8485284da6c096fb03d89d090c9886
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 14:00:12 -0500

    feat(subscriptionsandbilling): add amount value object.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Domain/Model/ValueObjects/Amount.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.ValueObjects;
+public record Amount
+{
+    public Amount() => Value = 0m;
+    public Amount(decimal value)
+    {
+        if (value < 0)
+            throw new ArgumentException("Amount must be greater than or equal to 0.", nameof(value));
+        Value = value;
+    }
+    public decimal Value { get; init; }
+}
