41743620299dae2d447afe154be68b85288c5033
vicmacode <victorgarciap10@hotmail.com>
2026-06-19 01:54:17 -0500

    feat(fleetoperations): add hauling cycle query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/FleetOperations/Application/QueryServices/IHaulingCycleQueryService.cs
@@ -0,0 +1,9 @@
+using GoldMetrics.GoldCheck.Platform.FleetOperations.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.FleetOperations.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.FleetOperations.Application.QueryServices;
+
+public interface IHaulingCycleQueryService
+{
+    Task<HaulingCycle?> Handle(GetHaulingCycleByIdQuery query, CancellationToken cancellationToken);
+}
