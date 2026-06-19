81f56c53b672a4f1b8b806a5747f9059a35e14a2
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 15:12:12 -0500

    feat(subscriptionsandbilling): add user subscription resource assembler.

---
+++ GoldMetrics.GoldCheck.Platform/SubscriptionsAndBilling/Interfaces/Rest/Transform/UserSubscriptionResourceFromEntityAssembler.cs
@@ -0,0 +1,15 @@
+﻿using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Domain.Model.Entities;
+using GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.SubscriptionsAndBilling.Interfaces.Rest.Transform;
+
+public static class UserSubscriptionResourceFromEntityAssembler
+{
+    public static UserSubscriptionResponse ToResponseFromEntity(UserSubscription entity) =>
+        new(entity.Id, entity.UserId.Value, entity.PlanType.Value, entity.BillingCycle.Value,
+            entity.SubscriptionStatus.Value, entity.AssignedFeatures, entity.AccessGranted, entity.Status);
+
+    public static InvoiceResponse ToInvoiceResponseFromEntity(Invoice invoice) =>
+        new(invoice.Id, invoice.InvoiceId.Value, invoice.Amount.Value, invoice.Status);
+}
