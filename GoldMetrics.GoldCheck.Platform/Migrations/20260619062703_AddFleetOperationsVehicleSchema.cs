4b05a087469c6907fc0342584b72bebcea003ac8
vicmacode <victorgarciap10@hotmail.com>
2026-06-19 01:28:19 -0500

    feat(fleetoperations): add persistence mapping for assign vehicle.

---
+++ GoldMetrics.GoldCheck.Platform/Migrations/20260619062703_AddFleetOperationsVehicleSchema.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace GoldMetrics.GoldCheck.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFleetOperationsVehicleSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vehicles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    vehicle_id = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    operator_id = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    is_engine_on = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    status = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_vehicles", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vehicles");
+        }
+    }
+}
