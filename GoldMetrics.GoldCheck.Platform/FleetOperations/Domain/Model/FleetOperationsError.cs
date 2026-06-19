3437f6b564a2af074507d121e7371acf4ac49ca4
vicmacode <victorgarciap10@hotmail.com>
2026-06-19 01:48:43 -0500

    feat(fleetoperations): add hauling cycle not found error.

--- GoldMetrics.GoldCheck.Platform/FleetOperations/Domain/Model/FleetOperationsError.cs
+++ GoldMetrics.GoldCheck.Platform/FleetOperations/Domain/Model/FleetOperationsError.cs
@@ -5,6 +5,7 @@ public enum FleetOperationsError
     None,
     VehicleNotFound,
     VehicleAlreadyAssigned,
+    HaulingCycleNotFound,
     OperationCancelled,
     DatabaseError,
     InternalServerError
