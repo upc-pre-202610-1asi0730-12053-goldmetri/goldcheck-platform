4b0ff000f63c3b250b663b05ce482691b241ae1d
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 09:47:38 -0500

    feat(monitoringtelemetry): add temperature reading aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Domain/Model/Aggregates/TemperatureReading.cs
@@ -0,0 +1,21 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+
+public partial class TemperatureReading
+{
+    public TemperatureReading() { AssetId = new AssetId(); Status = string.Empty; }
+
+    public TemperatureReading(MonitorEngineTemperatureCommand command)
+    {
+        AssetId = new AssetId(command.AssetId);
+        Status = "Monitoring";
+    }
+
+    public int Id { get; }
+    public AssetId AssetId { get; private set; }
+    public string Status { get; private set; }
+
+    public void ResetMonitoring() => Status = "Monitoring";
+}
