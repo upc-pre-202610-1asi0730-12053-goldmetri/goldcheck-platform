03f38c366fe46e29963947eb8a138d57b7219f6a
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 10:12:39 -0500

    feat(monitoringtelemetry): add exhaust temperature limit per cylinder analysed event.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Domain/Model/Events/ExhaustTemperatureLimitPerCylinderAnalysedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Events;
+
+public record ExhaustTemperatureLimitPerCylinderAnalysedEvent(string AssetId, decimal LimitCelsius, int CylinderNumber) : IEvent;
