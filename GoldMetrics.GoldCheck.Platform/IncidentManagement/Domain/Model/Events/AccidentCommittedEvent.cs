8da503ec60bf83a4fca9657c55b14b2255afde6d
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 22:20:50 -0500

    feat(incidentmanagement): add accident committed event.

---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Domain/Model/Events/AccidentCommittedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Events;
+
+public record AccidentCommittedEvent(string IncidentId, string OperatorId, string Description) : IEvent;
