6c7c9e3cce69081caa1e80462ca6476d9e06820b
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 22:03:48 -0500

    feat(incidentmanagement): add risk level alert committed event.

---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Domain/Model/Events/RiskLevelAlertCommittedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Events;
+
+public record RiskLevelAlertCommittedEvent(string IncidentId) : IEvent;
