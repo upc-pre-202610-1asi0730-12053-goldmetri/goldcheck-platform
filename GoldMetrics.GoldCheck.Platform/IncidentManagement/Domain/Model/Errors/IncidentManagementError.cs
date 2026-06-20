aaee9c934b7d6e2b5722d8647820d613e1b3a2a3
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 20:46:25 -0500

    feat(incidentmanagement): add base error enum and error catalog.

---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Domain/Model/Errors/IncidentManagementError.cs
@@ -0,0 +1,14 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Errors;
+
+public enum IncidentManagementError
+{
+    None,
+    IncidentNotFound,
+    InvalidRiskLevel,
+    InvalidIncidentType,
+    IncidentAlreadyCommitted,
+    RiskLevelValidationFailed,
+    OperationCancelled,
+    DatabaseError,
+    InternalServerError
+}
---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Domain/Model/Errors/IncidentManagementErrors.cs
@@ -0,0 +1,14 @@
+﻿namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Errors;
+
+public static class IncidentManagementErrors
+{
+    public static IncidentManagementError None => IncidentManagementError.None;
+    public static IncidentManagementError IncidentNotFound => IncidentManagementError.IncidentNotFound;
+    public static IncidentManagementError InvalidRiskLevel => IncidentManagementError.InvalidRiskLevel;
+    public static IncidentManagementError InvalidIncidentType => IncidentManagementError.InvalidIncidentType;
+    public static IncidentManagementError IncidentAlreadyCommitted => IncidentManagementError.IncidentAlreadyCommitted;
+    public static IncidentManagementError RiskLevelValidationFailed => IncidentManagementError.RiskLevelValidationFailed;
+    public static IncidentManagementError OperationCancelled => IncidentManagementError.OperationCancelled;
+    public static IncidentManagementError DatabaseError => IncidentManagementError.DatabaseError;
+    public static IncidentManagementError InternalServerError => IncidentManagementError.InternalServerError;
+}
