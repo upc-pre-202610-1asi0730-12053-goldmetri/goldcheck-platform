90e4da5f595ff84b815844a6006c8d20039fa6d2
genixmvp <genix.mvp@gmail.com>
2026-06-20 02:58:10 -0500

    feat(monitoringtelemetry): add gnss watchdog restart finished, chip restarted, and restart logged events.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Domain/Model/Events/GNSSRestartLoggedEvent.cs
@@ -0,0 +1,5 @@
+﻿using GoldMetrics.GoldCheck.Platform.Shared.Domain.Model.Events;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Events;
+
+public record GNSSRestartLoggedEvent(string AssetId) : IEvent;
