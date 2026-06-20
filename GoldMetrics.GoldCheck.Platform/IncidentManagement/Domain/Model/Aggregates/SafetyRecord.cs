d1034e130f6e9db1cbd29b168fbb7d4cbb6db50b
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 20:44:00 -0500

    feat(incidentmanagement): add safety record aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Domain/Model/Aggregates/SafetyRecord.cs
@@ -0,0 +1,32 @@
+﻿using GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Aggregates;
+
+public partial class SafetyRecord
+{
+    public SafetyRecord()
+    {
+        IncidentType = new IncidentType();
+        OperatorId = new OperatorId();
+        AssetId = new AssetId();
+        RiskLevel = new RiskLevel();
+        Status = string.Empty;
+    }
+
+    public SafetyRecord(DetectDriverFatigueCommand command)
+    {
+        IncidentType = new IncidentType("Fatigue");
+        OperatorId = new OperatorId(command.OperatorId);
+        AssetId = new AssetId(command.AssetId);
+        RiskLevel = new RiskLevel("Low");
+        Status = "FatigueDetected";
+    }
+
+    public int Id { get; }
+    public IncidentType IncidentType { get; private set; }
+    public OperatorId OperatorId { get; private set; }
+    public AssetId AssetId { get; private set; }
+    public RiskLevel RiskLevel { get; private set; }
+    public string Status { get; private set; }
+}
