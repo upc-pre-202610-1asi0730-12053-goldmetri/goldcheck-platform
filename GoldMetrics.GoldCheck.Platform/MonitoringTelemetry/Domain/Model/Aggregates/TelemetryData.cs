6344322e4a96765aa084b7753b23bdca5c8fb327
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 23:11:41 -0500

    feat(monitoringtelemetry): add telemetry data aggregate skeleton.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Domain/Model/Aggregates/TelemetryData.cs
@@ -0,0 +1,25 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.ValueObjects;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+
+public partial class TelemetryData
+{
+    public TelemetryData() { AssetId = new AssetId(); RawData = string.Empty; TelemetryDataId = string.Empty; Status = string.Empty; }
+
+    public TelemetryData(ProcessTelemetryDataCommand command)
+    {
+        AssetId = new AssetId(command.AssetId);
+        TelemetryDataId = Guid.NewGuid().ToString();
+        RawData = command.RawData;
+        Status = "Processed";
+        IsValidated = false;
+    }
+
+    public int Id { get; }
+    public AssetId AssetId { get; private set; }
+    public string TelemetryDataId { get; private set; }
+    public string RawData { get; private set; }
+    public string Status { get; private set; }
+    public bool IsValidated { get; private set; }
+}
