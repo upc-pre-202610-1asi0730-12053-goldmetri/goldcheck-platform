ef55b5742d9bad8813b1b7ec4e245a91885bb306
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 10:47:27 -0500

    feat(monitoringtelemetry): add temperature query service contract.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/QueryServices/ITemperatureQueryService.cs
@@ -0,0 +1,9 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Queries;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.QueryServices;
+
+public interface ITemperatureQueryService
+{
+    Task<IEnumerable<TemperatureReading>> Handle(GetTemperatureReadingByAssetQuery query, CancellationToken cancellationToken = default);
+}
