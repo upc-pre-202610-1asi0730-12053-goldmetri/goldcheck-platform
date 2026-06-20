f264e85131bb62a2707f6f0a7cd4059a8ef69b5b
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 11:19:18 -0500

    feat(monitoringtelemetry): add detect communication anomaly command handling implementation (includes internal log communication anomaly).

--- GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/Internal/CommandServices/CommunicationCommandService.cs
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/Internal/CommandServices/CommunicationCommandService.cs
@@ -69,4 +69,35 @@
                return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.InternalServerError, localizer[nameof(MonitoringTelemetryError.InternalServerError)]);
            }
        }
+       
+       public async Task<Result<CommunicationChannel>> Handle(DetectCommunicationAnomalyCommand command, CancellationToken cancellationToken = default)
+       {
+           var channels = await repository.FindByAssetIdAsync(command.AssetId, cancellationToken);
+           var channel = channels.MaxBy(c => c.CreatedAt);
+           if (channel is null)
+               return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.CommunicationChannelNotFound, localizer[nameof(MonitoringTelemetryError.CommunicationChannelNotFound)]);
+           try
+           {
+               channel.DetectAnomaly(command);
+               channel.LogAnomaly(new LogCommunicationAnomalyCommand(command.AssetId, command.Protocol, command.AnomalyDescription));
+               await unitOfWork.CompleteAsync(cancellationToken);
+               return Result<CommunicationChannel>.Success(channel);
+           }
+           catch (ArgumentException)
+           {
+               return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.InvalidProtocol, localizer[nameof(MonitoringTelemetryError.InvalidProtocol)]);
+           }
+           catch (OperationCanceledException)
+           {
+               return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.OperationCancelled, localizer[nameof(MonitoringTelemetryError.OperationCancelled)]);
+           }
+           catch (DbUpdateException)
+           {
+               return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.DatabaseError, localizer[nameof(MonitoringTelemetryError.DatabaseError)]);
+           }
+           catch (Exception)
+           {
+               return Result<CommunicationChannel>.Failure(MonitoringTelemetryError.InternalServerError, localizer[nameof(MonitoringTelemetryError.InternalServerError)]);
+           }
+       }
    }
