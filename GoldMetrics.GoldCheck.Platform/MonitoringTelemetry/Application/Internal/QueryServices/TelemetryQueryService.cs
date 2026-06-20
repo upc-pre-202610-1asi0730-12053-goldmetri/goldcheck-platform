f77b8632416758fa60d06923f189448cb3601cbe
genixmvp <genix.mvp@gmail.com>
2026-06-20 02:22:23 -0500

    feat(monitoringtelemetry): add get telemetry data by asset query handling implementation.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/Internal/QueryServices/TelemetryQueryService.cs
@@ -0,0 +1,12 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.QueryServices;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Queries;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.Internal.QueryServices;
+
+public class TelemetryQueryService(ITelemetryDataRepository repository) : ITelemetryQueryService
+{
+    public async Task<IEnumerable<TelemetryData>> Handle(GetTelemetryDataByAssetQuery query, CancellationToken cancellationToken = default)
+        => await repository.FindByAssetIdAsync(query.AssetId, cancellationToken);
+}
