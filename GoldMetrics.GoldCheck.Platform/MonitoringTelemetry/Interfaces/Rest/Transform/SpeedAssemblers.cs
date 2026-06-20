47c46e524274f1393a41329080e4be6d17eeb6de
kattyph <katty.philco17@gmail.com>
2026-06-20 06:23:10 -0500

    feat(monitoring-telemetry): add speed reading resource assembler.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Interfaces/Rest/Transform/SpeedAssemblers.cs
@@ -0,0 +1,19 @@
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Interfaces.Rest.Transform;
+
+public static class MonitorSpeedCommandFromResourceAssembler
+{
+    public static MonitorSpeedStatusCommand ToCommandFromResource(MonitorSpeedStatusResource resource)
+        => new(resource.AssetId);
+}
+
+public static class SpeedReadingResourceFromEntityAssembler
+{
+    public static SpeedReadingResource ToResourceFromEntity(SpeedReading entity)
+        => new(entity.Id, entity.AssetId.Value, entity.Status,
+            entity.CurrentSpeedKmPerHour, entity.SpeedLimitKmPerHour, entity.IsViolation,
+            entity.ViolationDescription, entity.CreatedAt, entity.UpdatedAt);
+}
