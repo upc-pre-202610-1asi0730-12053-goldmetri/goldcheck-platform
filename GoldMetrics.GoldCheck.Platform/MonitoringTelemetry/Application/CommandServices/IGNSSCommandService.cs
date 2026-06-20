39b2b1600ec1c47c6757f9b182051fc6327928aa
genixmvp <genix.mvp@gmail.com>
2026-06-20 02:29:09 -0500

    feat(monitoringtelemetry): add gnss command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/CommandServices/IGNSSCommandService.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.CommandServices;
+
+public interface IGNSSCommandService
+{
+    Task<Result<GNSSStatus>> Handle(MonitorGNSSStatusCommand command, CancellationToken cancellationToken = default);
+}
