999050eb239c8fdd4e7fc1d6ad829f6426f4553a
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 11:22:55 -0500

    feat(monitoringtelemetry): add get communication channel by asset query handling implementation.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/Internal/QueryServices/CommunicationQueryService.cs
@@ -0,0 +1,12 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.QueryServices;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Queries;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Repositories;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.Internal.QueryServices;
+
+public class CommunicationQueryService(ICommunicationChannelRepository repository) : ICommunicationQueryService
+{
+    public async Task<IEnumerable<CommunicationChannel>> Handle(GetCommunicationChannelByAssetQuery query, CancellationToken cancellationToken = default)
+        => await repository.FindByAssetIdAsync(query.AssetId, cancellationToken);
+}
