e8a8e70ff7d789842fcd09d36a05d2609d0079a1
genixmvp <genix.mvp@gmail.com>
2026-06-20 02:35:36 -0500

    feat(monitoringtelemetry): add gnss status resource assembler.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Interfaces/Rest/Transform/GNSSAssemblers.cs
@@ -0,0 +1,19 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Interfaces.Rest.Transform;
+
+public static class MonitorGNSSCommandFromResourceAssembler
+{
+    public static MonitorGNSSStatusCommand ToCommandFromResource(MonitorGNSSStatusResource resource)
+        => new(resource.AssetId);
+}
+
+public static class GNSSStatusResourceFromEntityAssembler
+{
+    public static GNSSStatusResource ToResourceFromEntity(GNSSStatus entity)
+        => new(entity.Id, entity.AssetId.Value, entity.Status,
+            entity.RestartReason, entity.RestartCount,
+            entity.CreatedAt, entity.UpdatedAt);
+}
