e00ad0546399e5f9266ae8f6553c7717dad9b5a6
Adrian5102 <u202410084@upc.edu.pe>
2026-06-20 11:04:57 -0500

    feat(monitoringtelemetry): add communication command service contract.

---
+++ GoldMetrics.GoldCheck.Platform/MonitoringTelemetry/Application/CommandServices/ICommunicationCommandService.cs
@@ -0,0 +1,10 @@
+﻿using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Domain.Model.Commands;
+using GoldMetrics.GoldCheck.Platform.Shared.Application.Model;
+
+namespace GoldMetrics.GoldCheck.Platform.MonitoringTelemetry.Application.CommandServices;
+
+public interface ICommunicationCommandService
+{
+    Task<Result<CommunicationChannel>> Handle(MonitorCommunicationChannelCommand command, CancellationToken cancellationToken = default);
+}
