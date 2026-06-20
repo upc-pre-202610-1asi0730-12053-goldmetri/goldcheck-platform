1a89303c7b9b8d4b30005de866d099962673e24c
kitu05g <kiaratuesta25@gmail.com>
2026-06-19 21:15:17 -0500

    feat(incidentmanagement): add safety record resource assembler and action result assembler.

---
+++ GoldMetrics.GoldCheck.Platform/IncidentManagement/Interfaces/Rest/Transform/SafetyRecordResourceFromEntityAssembler.cs
@@ -0,0 +1,17 @@
+﻿using GoldMetrics.GoldCheck.Platform.IncidentManagement.Domain.Model.Aggregates;
+using GoldMetrics.GoldCheck.Platform.IncidentManagement.Interfaces.Rest.Resources;
+
+namespace GoldMetrics.GoldCheck.Platform.IncidentManagement.Interfaces.Rest.Transform;
+
+public static class SafetyRecordResourceFromEntityAssembler
+{
+    public static SafetyRecordResource ToResourceFromEntity(SafetyRecord entity) =>
+        new(
+            entity.Id,
+            entity.IncidentType.Value,
+            entity.OperatorId.Value,
+            entity.AssetId.Value,
+            entity.RiskLevel.Value,
+            entity.Description,
+            entity.Status);
+}
